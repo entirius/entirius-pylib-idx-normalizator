476e4df2c0ee30cdabb31b9551368084ddb05e57
Piotr Brzozowski <piotrb@ecom.house>
2025-07-10 20:37:49 +0200

    test fixes, formating

--- src/idx_normalizator/idx_normalizator.py
+++ src/idx_normalizator/idx_normalizator.py
@@ -18,18 +18,20 @@ def normalize_idx(text: str, max_len: int = 128) -> str:
         m = hashlib.md5()
         m.update(rest)
         h = m.hexdigest()
-        idx = "%s%s%s" % (prefix, separator, h[:hash_len])
+        idx = f"{prefix}{separator}{h[:hash_len]}"
     return idx
 
 
 def validate_idx(idx: str, min_len=1, max_len=128):
     idx_normalized = normalize_idx(idx, max_len=max_len)
     if idx_normalized != idx:
-        raise ValueError("idx must be slugify, maybe use normalize_idx()? {} != {}".format(idx_normalized, idx))
+        raise ValueError(
+            f"idx must be slugify, maybe use normalize_idx()? {idx_normalized} != {idx}"
+        )
     if len(idx) < min_len:
-        raise ValueError("idx must be {} min chars, idx={}".format(min_len, idx))
+        raise ValueError(f"idx must be {min_len} min chars, idx={idx}")
     if len(idx) > max_len:
-        raise ValueError("idx must be {} max chars, idx={}".format(max_len, idx))
+        raise ValueError(f"idx must be {max_len} max chars, idx={idx}")
 
 
 #
@@ -50,15 +52,19 @@ def validate_sku(sku):
     forbidden_chars = [","]
     sku_normalized = normalize_sku(sku)
     if sku_normalized != sku:
-        raise ValueError("sku must be striped, maybe use normalize_sku()? {} != {}".format(sku_normalized, sku))
+        raise ValueError(
+            f"sku must be striped, maybe use normalize_sku()? {sku_normalized} != {sku}"
+        )
     for char in forbidden_chars:
         index = sku.find(char)
         if index != -1:
-            raise ValueError('SKU contains forbidden character="{}" sku={}'.format(char, sku))
+            raise ValueError(
+                f'SKU contains forbidden character="{char}" sku={sku}'
+            )
     if len(sku) < min_len:
-        raise ValueError("SKU must be {} min chars, sku={}".format(min_len, sku))
+        raise ValueError(f"SKU must be {min_len} min chars, sku={sku}")
     if len(sku) > max_len:
-        raise ValueError("SKU must be {} max chars, sku={}".format(max_len, sku))
+        raise ValueError(f"SKU must be {max_len} max chars, sku={sku}")
 
 
 def normalize_ean(ean):
@@ -72,11 +78,14 @@ def validate_ean(ean, min_len=8, max_len=16):
         return
     ean_normalized = normalize_ean(ean)
     if ean_normalized != ean:
-        raise ValueError("ean must be lower and strip, maybe use normalize_ean()? {} != {}".format(ean_normalized, ean))
+        raise ValueError(
+            f"ean must be lower and strip, maybe use normalize_ean()? "
+            f"{ean_normalized} != {ean}"
+        )
     if len(ean) < min_len:
-        raise ValueError("ean must be {} min chars, ean={}".format(min_len, ean))
+        raise ValueError(f"ean must be {min_len} min chars, ean={ean}")
     if len(ean) > max_len:
-        raise ValueError("ean must be {} max chars, ean={}".format(max_len, ean))
+        raise ValueError(f"ean must be {max_len} max chars, ean={ean}")
 
 
 def normalize_url_key(url_key):
@@ -95,7 +104,6 @@ def validate_url_key(url_key):
     url_key = normalize_url_key(url_key)
     if url_key != orig:
         raise Exception(
-            'ProductCategory "url_key" must be striped and lowered and slugified, maybe use normalize_url_key()? {} != {}'.format(
-                url_key, orig
-            )
+            f'ProductCategory "url_key" must be striped and lowered and '
+            f"slugified, maybe use normalize_url_key()? {url_key} != {orig}"
         )
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,18 +1,17 @@
 import unittest
 
-from idx_normalizator import (
-    normalize_idx, 
-    validate_idx
-)
+from idx_normalizator import normalize_idx
+
 
 class TestValidation(unittest.TestCase):
     def test_normalize_idx(self):
-        names={
+        names = {
             "Example Cammel Name": "example-cammel-name",
         }
         for name, idx in names.items():
             nornalized = normalize_idx(name)
             self.assertEqual(nornalized, idx)
+
 
 if __name__ == "__main__":
     unittest.main()
